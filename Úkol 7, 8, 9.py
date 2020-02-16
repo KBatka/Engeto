55bd2658081f8a0fff9e487cc3b7c447f425cacc
Katka Baťková <katerinaBatkova@seznam.cz>
2020-02-16 15:29:16 +0100

    Postnutí zadání 7,8,9. úkolu

---
+++ Úkol 7, 8, 9.py
@@ -0,0 +1,75 @@
+#Úkol 7
+
+# Create Candidate
+
+
+# Print candidates at the beginning
+
+
+# Create employees
+
+
+# Print employees at the beginning
+
+
+# Add new candidates
+
+
+# Print new candidates
+
+
+# Insert name
+
+
+# Print the employees list after entering a new name
+
+#Úkol 8
+# Results from previous task
+candidates = ['Bruno', 'Agnes']
+employees = ['Francis', 'Bruno', 'Ann', 'Jacob', 'Claire']
+
+
+
+# Delete names from candidates
+
+
+# Print remaining candidates
+
+
+# Repeat element
+
+
+# Print repeating element in list candidates
+
+
+# Join lists
+
+
+# Print joined lists
+
+
+# Index 2
+
+
+# Find out last index and assign it to variable
+
+
+#Úkol 9
+# Results from the previous task
+candidates = ['Agnes', 'Agnes', 'Agnes']
+employees = ['Francis', 'Bruno', 'Ann', 'Jacob', 'Claire', 'Agnes', 'Agnes', 'Agnes']
+
+
+# Interval 2-5
+
+
+# Each 3rd
+
+
+# Save index
+
+
+# Jacob index
+
+
+# Number of name Agnes
