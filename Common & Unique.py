1541261a85907f81610fec308e588511b6cff446
Katka Baťková <katerinaBatkova@seznam.cz>
2020-02-17 12:25:36 +0100

    sym. diff. + intersection, sets

--- Common & Unique.py
+++ Common & Unique.py
@@ -5,7 +5,7 @@ set(string01)
 set(string02)
 
 common_characters = set(string01) & set(string02)
-unique_characters = set(string01) ^ set(string02)
+unique_characters = set(string01) - set(string02)
 
 print("common characters", common_characters)
 print("unique characters", unique_characters)
