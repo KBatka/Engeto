8319d60d9da647ec69cb4dab636113d34fdbdf74
Katka Baťková <katerinaBatkova@seznam.cz>
2020-02-17 17:56:17 +0100

    úkol druhá lekce, dokončení rozpracovaného úkolu

---
+++ ukoldruhalekce.py
@@ -0,0 +1,14 @@
+#variable_name = expression1 if condition else expression2
+
+my_str = 'Python'
+result = ''
+
+if my_str.istitle():
+    result = print('Titlecased')
+else:
+    result = print('Not titlecased')
+
+my_str = 'Python'
+result = print('Titlecased') if my_str.istitle() else print('Not titlecased')
+
+#ptá se mne, zda je první písmeno velké, nebo všechna?
