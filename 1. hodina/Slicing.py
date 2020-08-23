9e6c35fb4279a4f96bcd666f1689c7448b1e443e
Katka Baťková <katerinaBatkova@seznam.cz>
2020-08-23 14:30:13 +0200

    slicing

---
+++ 1. hodina/Slicing.py
@@ -0,0 +1,9 @@
+lol = "indexing"
+
+first_five = lol[0:5]
+last_five = lol[3:8]
+each_third = lol[0:8:3]
+
+print("First five letters:" + first_five)
+print("Last five letters:" + last_five)
+print("Each thir letter:" + each_third)
