e75b6ef6924546778db7248744d70e61cb717f21
Katka Baťková <katerinaBatkova@seznam.cz>
2020-08-31 17:57:11 +0200

    řešení

--- 3.lekce/Tram stations.py
+++ 3.lekce/Tram stations.py
@@ -3,15 +3,11 @@ tram_stations = {
 'No.2' : ['Zidenice', 'Kuldova', 'Vojenska nemocnice', 'Tkalcovska', 'Kornerova', 'Malinovske nam.', 'Hlavni nadr.', 'Nove Sady', 'Hybesova', 'Vaclavska', 'Mendlovo nam.', 'Porici', 'Nemocnice UM', 'Celni', 'Hluboka', 'Ustredni hrbitov'],
 'No.3' : ['Husovice','Nam. republiky','Vozovna Husovice','Mostecka','Travnickova', 'Tkalcovska', 'Kornerova', 'Malinovske nam.', 'Hlavni nadr.', 'Nove sady', 'Silingrovo nam.', 'Ceska', 'Komenskeho nam.', 'Obilni trh', 'Uvoz']
 }
-# Our goal is to identify which stations are included in each list.
-# Stanice, které leží v každém listu
-# aneb jak dodat "grammarnazi slovnik" - interpunkce a překlepy
 
-# & intersection
-# udělat z linek sety -> linka = název setu, z hodnot slovníků set, klíč jako název fce setu
-# rozpracováno
+prvni_linka = set(tram_stations.get('No.1'))
+druha_linka = set(tram_stations.get('No.2'))
+treti_linka = set(tram_stations.get('No.3'))
 
-set
+prunik = prvni_linka & druha_linka & treti_linka
 
-namka = set(tram_stations)
-print(namka)
+print(prunik)
